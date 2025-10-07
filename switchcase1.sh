70b22f7e2da106d8c1915c5d2178d168e76b3fd0
felitech25 <etech.felix+dev@gmail.com>
2025-10-07 01:43:50 -0500

    first switchcase added

---
+++ switchcase1.sh
@@ -0,0 +1,14 @@
+echo "Enter a number between 1 and 3:"
+read number
+
+case $number in
+  1)
+    echo "You selected One."
+    ;;
+  2)
+    echo "You selected Two."
+    ;;
+  3)
+    echo "You selected Three."
+    ;;
+  *)
